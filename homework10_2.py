4077aac033c084d7608862dbde87b69ccd6dba96
Віталій <git@github.com:Vandalij/My-begin.git>
2025-06-02 14:36:06 +0200

    homework10 and 11

---
+++ homework10_2.py
@@ -0,0 +1,15 @@
+from re import search
+
+
+def first_word(text):
+    """ Пошук першого слова """
+    match = search("[a-zA-Z]+", text)
+    return match.group(0) if match else ''
+
+assert first_word("Hello world") == "Hello", 'Test1'
+assert first_word("greetings, friends") == "greetings", 'Test2'
+assert first_word("don't touch it") == "don't", 'Test3'
+assert first_word(".., and so on ...") == "and", 'Test4'
+assert first_word("hi") == "hi", 'Test5'
+assert first_word("Hello.World") == "Hello", 'Test6'
+print('OK')
